2dfc7fb4c41d7b1c145ebb6d726c2de9906fd965
imseongsu <in05013300@gmail.com>
2025-01-05 21:28:59 +0900

    PgaeTransitions

---
+++ app.js
@@ -0,0 +1,9 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controlls');
+const sectBth = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main-content');
+
+function PageTransitions() {
+    // Button click active class
+    
+}
